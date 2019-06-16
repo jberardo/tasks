22d86d6736cf9d9301b561d04e02ec4a9e575782
Joao Berardo <jberardo@me.com>
2019-06-16 03:41:43 -0700

    angular task-list component

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Task } from "../task.model";
 
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.css']
+  selector: "app-tasks-list",
+  templateUrl: "./tasks-list.component.html",
+  styleUrls: ["./tasks-list.component.css"]
 })
 export class TasksListComponent implements OnInit {
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.tasks.push(new Task(1, "Task 1", true, "07/05/2019"));
+    this.tasks.push(new Task(2, "Task 2", false, "04/05/2019"));
+    this.tasks.push(new Task(3, "Task 3", true, "02/05/2019"));
   }
 
+  getDueDateLabel(task: Task) {
+    return task.completed ? "badge-success" : "badge-primary";
+  }
+
+  onTaskChange(event, task) {
+    console.log('Task has changed.')
+  }
 }
